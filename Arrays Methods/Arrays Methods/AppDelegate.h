fa32e7a8c6ee2e4f2e3e99b5d8e0c5bfacda22f9
Georg <azurit93@mail.ru>
2015-07-11 13:01:34 +0300

    Test

---
+++ Arrays Methods/Arrays Methods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Arrays Methods
+//
+//  Created by Georg on 11.07.15.
+//  Copyright (c) 2015 Georg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Arrays Methods/Arrays Methods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Arrays Methods
+//
+//  Created by Georg on 11.07.15.
+//  Copyright (c) 2015 Georg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
